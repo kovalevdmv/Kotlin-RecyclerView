336a062747e5bd134f8da3ce491ea64131905201
kovalevdmv@yandex.ru <kovalevdmv@yandex.ru>
2019-09-15 17:30:41 +0300

    test

--- app/src/main/java/com/example/myapplication/Main2Activity.kt
+++ app/src/main/java/com/example/myapplication/Main2Activity.kt
@@ -17,7 +17,6 @@ class Main2Activity : AppCompatActivity() {
 
 
         rec_view.layoutManager = LinearLayoutManager(this)
-
         //rec_view.layoutManager = GridLayoutManager(this, 2)
 
         rec_view.adapter = MyAdapter(l, this)
